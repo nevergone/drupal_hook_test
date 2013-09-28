ba321a5a6dab7a935f636d6d118df57543563bcb
Kurucz István <kurucz.istvan@gmail.com>
2013-09-28 21:04:30 +0200

    module_invoke_all() használata a hook-függvények meghívásához.
    A hook függvények nem módosítják a paraméterként átadott értéket, a visszaadott értékeket tömbben kapjuk meg.

---
+++ simplepage/simplepage.api.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Simplepage modul által meghívott hook függvények dokumentációja
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * A "module_invoke_all()" függvény teszteléséhez használt hook.
+ *
+ * @param $text Felhasználó által megadott, változóban eltárolt szöveg
+ *
+ * @return A hook meghívása után üzenetként megjelenített szöveg.
+ */
+function hook_simplepage_teszt($text) {
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
+
